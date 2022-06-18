93f2e36e32ad083393bc3946626ef16b2a7c581e
azizsige <kurniawan00azizsige@gmail.com>
2022-06-18 16:49:50 +0700

    add: postcss-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,9 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    "postcss-preset-env": {
+      features: { "nesting-rules": false },
+    },
+    ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
   },
 };
